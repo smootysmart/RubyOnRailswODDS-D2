a0396fec04bb5f34c9808f55f1460caa082e9969
smootysmart <noppakrit.niti@mail.kmutt.ac.th>
2024-11-03 13:46:55 +0700

    add Delete , Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,14 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def show 
+  def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
     @article = Article.new
   end
-  
+
   def create
     @article = Article.new(article_params)
 
@@ -20,10 +20,30 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
-  private
-  def article_params
-    params.require(:article).permit(:title, :body)
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
